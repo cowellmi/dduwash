d48ea5ff6cf2c2c6dc791fb323344fb3aa9e42d1
Micah Cowell <micahcowell@gmail.com>
2026-04-21 10:46:44 -0700

    build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
     root: 'frontend',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'frontend/index.html'),
+                es: resolve(__dirname, 'frontend/es/index.html'),
+            },
+        },
+    },
     server: {
         proxy: {
             '/api': {
@@ -8,4 +19,4 @@ export default {
             }
         }
     }
-}
+});
